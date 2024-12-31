1952a8ef77cbd5684f120d053fcada87f99d2881
thanh.tran <thanh.trannhat@digime.asia>
2024-12-31 17:06:04 +0700

    Chore

--- app/geocoding/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from .v1 import geocoding_v1 
-
-geocoding_router = APIRouter(prefix='/api', tags=['GEOCODING'])
-geocoding_router.include_router(geocoding_v1, prefix='/v1')
--- deployment/prod.webserver.sh
+++
@@ -1,4 +0,0 @@
-sudo docker stop $(sudo docker ps -a -q --filter ancestor=prod-webserver) && sudo docker rm $(sudo docker ps -a -q --filter ancestor=prod-webserver)
-sudo docker build -t prod-webserver .
-sudo docker run -d --name prod-webserver --network dockers_dms-network -p 8000:8000 -v $(pwd)/logs:/code/logs --env-file .env prod-webserver
-sudo docker logs -f -n 100 prod-webserver
